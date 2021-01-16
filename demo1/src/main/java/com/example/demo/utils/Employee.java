c481db9063ee68a02d0a129977563f425714db85
V919 <76912566+V919@users.noreply.github.com>
2021-01-17 00:47:42 +0530

    1
    pushing demo1 project to beginers book repository it is initial commit

---
+++ demo1/src/main/java/com/example/demo/utils/Employee.java
@@ -0,0 +1,16 @@
+package com.example.demo.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+	private Integer eid;
+	private String ename;
+	private Double sal;
+	private String name;
+
+}
